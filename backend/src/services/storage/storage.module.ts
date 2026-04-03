2207a42459845f9b1f65902ba65f6f031ec9bc2b
superUserT <rantshothabisomail@gmail.com>
2026-04-04 01:09:28 +0200

    added controller for mimio file upload logic

--- backend/src/services/storage/storage.module.ts
+++ backend/src/services/storage/storage.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MinioStorageService } from './minio-storage/minio-storage.service';
+import { I_STORAGE_PROVIDER } from './storage.provider.interface';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: I_STORAGE_PROVIDER,
+      useClass: MinioStorageService,
+    },
+  ],
+  exports: [I_STORAGE_PROVIDER], 
+})
 export class StorageModule {}
